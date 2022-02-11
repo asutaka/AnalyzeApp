92b077dcbbf1a039038f6b40d88cff3288051195
phunv@ftech.ai <phunv@ftech.ai>
2022-02-11 13:21:39 +0700

    update storedData

--- AnalyzeApp.Common/DataMng.cs
+++ AnalyzeApp.Common/DataMng.cs
@@ -1,9 +1,15 @@
 ﻿using AnalyzeApp.Model.ENTITY;
 using AnalyzeApp.Model.ENUM;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AnalyzeApp.Common
 {
@@ -34,6 +40,158 @@ namespace AnalyzeApp.Common
                 return null;
             }
         }
+        #region StoredData
+        public static async Task StoredData()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            client.BaseAddress = new Uri(ConstVal.api);
+            try
+            {
+                //update flag
+                var model = new ConfigTableModel { StatusLoadData = (int)enumStatusLoadData.Loading };
+                HttpContent c = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+                await client.PostAsync("Setting/UpdateConfigTable", c);
+                //
+                var responseDataSetting = await client.GetAsync("Setting/GetDataSettings");
+                if (responseDataSetting != null)
+                {
+                    var lDataSetting = JsonConvert.DeserializeObject<IEnumerable<DataSettingModel>>(await responseDataSetting.Content.ReadAsStringAsync());
+                    if (lDataSetting != null)
+                    {
+                        var lstTask = new List<Task>();
+                        long curTime = DateTimeOffset.Now.ToUnixTimeMilliseconds();
+                        foreach (var item in lDataSetting)
+                        {
+                            var task = Task.Run(() =>
+                            {
+                                var div = curTime - item.UpdatedTime;
+                                switch (item.Interval)
+                                {
+                                    case (int)enumInterval.ThirteenMinute:
+                                        {
+                                            if (div > 900000)
+                                            {
+                                                SyncDataInterval(enumInterval.ThirteenMinute);
+                                            }
+                                            break;
+                                        }
+                                    case (int)enumInterval.OneHour:
+                                        {
+                                            if (div > 3600000)
+                                            {
+                                                SyncDataInterval(enumInterval.OneHour);
+                                            }
+                                            break;
+                                        }
+                                    case (int)enumInterval.FourHour:
+                                        {
+                                            if (div > 14400000)
+                                            {
+                                                SyncDataInterval(enumInterval.FourHour);
+                                            }
+                                            break;
+                                        }
+                                    case (int)enumInterval.OneDay:
+                                        {
+                                            if (div > 86400000)
+                                            {
+                                                SyncDataInterval(enumInterval.OneDay);
+                                            }
+                                            break;
+                                        }
+                                    case (int)enumInterval.OneWeek:
+                                        {
+                                            if (div > 604800000)
+                                            {
+                                                SyncDataInterval(enumInterval.OneWeek);
+                                            }
+                                            break;
+                                        }
+                                    case (int)enumInterval.OneMonth:
+                                        {
+                                            if (div > 2419200000)
+                                            {
+                                                SyncDataInterval(enumInterval.OneMonth);
+                                            }
+                                            break;
+                                        }
+                                }
+                                var modelTime = new DataSettingModel { Interval = item.Interval, UpdatedTime = curTime };
+                                HttpContent content = new StringContent(JsonConvert.SerializeObject(modelTime), Encoding.UTF8, "application/json");
+                                client.PostAsync("Setting/UpdateDataSettings", content).GetAwaiter().GetResult();
+                            });
+                            lstTask.Add(task);
+                        }
+                        Task.WaitAll(lstTask.ToArray());
+                        //update flag
+                        model.StatusLoadData = (int)enumStatusLoadData.Endload;
+                        c = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
+                        await client.PostAsync("Setting/UpdateConfigTable", c);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                NLogLogger.PublishException(ex, $"DataMng|LoadData:{ex.Message}");
+            }
+        }
+        private static void SyncDataInterval(enumInterval interval)
+        {
+            try
+            {
+                var lCoin = GetCoin();
+                var lstTask = new List<Task>();
+                foreach (var item in lCoin)
+                {
+                    var task = Task.Run(() =>
+                    {
+                        SyncDataValue(item.S, interval);
+                    });
+                    lstTask.Add(task);
+                }
+                Task.WaitAll(lstTask.ToArray());
+            }
+            catch (Exception ex)
+            {
+                NLogLogger.PublishException(ex, $"DataMng|SyncDataInterval: {ex.Message}");
+            }
+        }
+        private static void SyncDataValue(string coin, enumInterval interval)
+        {
+            try
+            {
+                var pathFile = $"{Directory.GetCurrentDirectory()}\\data\\{interval.GetDisplayName()}\\{coin}.txt";
+                JArray arrSource;
+                var index = 0;
+                do
+                {
+                    arrSource = GetSource(coin, interval);
+                }
+                while (index < 3 && arrSource == null);
+                if (arrSource == null)
+                {
+                    return;
+                }
+                //
+                File.Create(pathFile).Close();
+                //
+                using (StreamWriter stream = new StreamWriter(pathFile, true))
+                {
+                    foreach (var line in arrSource)
+                    {
+                        stream.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                NLogLogger.PublishException(ex, $"DataMng|SyncDataValue: {ex.Message}");
+            }
+        }
+        #endregion
     }
 }
 //15m: 10 ngày
--- AnalyzeApp/GUI/frmLogin.cs
+++ AnalyzeApp/GUI/frmLogin.cs
@@ -1,15 +1,11 @@
 ﻿using AnalyzeApp.Common;
 using AnalyzeApp.Model.ENTITY;
-using AnalyzeApp.Model.ENUM;
 using DevExpress.XtraEditors;
 using Newtonsoft.Json;
 using System;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.IO;
 using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Text;
 using System.Threading;
 using System.Windows.Automation;
@@ -23,7 +19,13 @@ namespace AnalyzeApp.GUI
         private frmLogin()
         {
             InitializeComponent();
-            Init();
+            var bkgr = new BackgroundWorker();
+            bkgr.DoWork += (object sender, DoWorkEventArgs e) =>
+            {
+                Startup.Instance();
+                DataMng.StoredData().GetAwaiter().GetResult();
+            };
+            bkgr.RunWorkerAsync();
         }
 
         private static frmLogin _instance = null;
@@ -31,26 +33,6 @@ namespace AnalyzeApp.GUI
         {
             _instance = _instance ?? new frmLogin();
             return _instance;
-        }
-
-        private void Init()
-        {
-            HttpClient client = new HttpClient();
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Accept.Add(
-                new MediaTypeWithQualityHeaderValue("application/json"));
-            client.BaseAddress = new Uri(ConstVal.api);
-            var model = new ConfigTableModel { StatusLoadData = (int)enumStatusLoadData.None };
-            HttpContent c = new StringContent(JsonConvert.SerializeObject(model), Encoding.UTF8, "application/json");
-            client.PostAsync("Setting/UpdateConfigTable", c).GetAwaiter().GetResult();
-            //
-            Process.Start($"{Directory.GetCurrentDirectory()}\\dataservice\\{ConstVal.dataServiceName}.exe");
-            var bkgr = new BackgroundWorker();
-            bkgr.DoWork += (object sender, DoWorkEventArgs e) =>
-            {
-                Startup.Instance();
-            };
-            bkgr.RunWorkerAsync();
         }
 
         private void picGoogleSignIn_Click(object sender, EventArgs e)
