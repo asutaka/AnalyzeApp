d5e0b77817364d02f94af0fa03a0cb865ff0ceb1
phunv@ftech.ai <phunv@ftech.ai>
2022-03-01 17:34:32 +0700

    update subcribe

--- AnalyzeApp/StartUp.cs
+++ AnalyzeApp/StartUp.cs
@@ -1,19 +1,17 @@
 ﻿using AnalyzeApp.Common;
 using AnalyzeApp.Job;
 using AnalyzeApp.Job.ScheduleJob;
-using Newtonsoft.Json;
 using Quartz;
 using System;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace AnalyzeApp
 {
     public class StartUp
     {
-        public static async Task Load()
+        public static void Load()
         {
             try
             {
@@ -23,53 +21,64 @@ namespace AnalyzeApp
                 new ScheduleMember(StaticVal.scheduleMng.GetScheduler(), JobBuilder.Create<Data1DJob>(), "0 0 0 * * ?", nameof(Data1DJob)).Start();
                 new ScheduleMember(StaticVal.scheduleMng.GetScheduler(), JobBuilder.Create<Data1WJob>(), "0 0 0 1/7 * ?", nameof(Data1WJob)).Start();
                 new ScheduleMember(StaticVal.scheduleMng.GetScheduler(), JobBuilder.Create<Data1MJob>(), "0 0 0 1 * ?", nameof(Data1MJob)).Start();
-
-                var ticket = (await StaticVal.binanceClient.Spot.Market.GetTickersAsync()).Data.ToList();
-                StaticVal.binanceTicks = ticket;
-                var binanceTick = ticket;
-                var subscribeResult = StaticVal.binanceSocketClient.Spot.SubscribeToAllSymbolTickerUpdatesAsync(data => {
-                    // Handle data when it is received
-                    var lExists = binanceTick.Where(x => data.Data.Any(y => y.Symbol == x.Symbol));
-                    if (lExists != null && lExists.Any())
-                    {
-                        binanceTick = binanceTick.Except(lExists).ToList();
-                    }
-                    binanceTick.AddRange(data.Data);
-                    StaticVal.binanceTicks = binanceTick;
-                });
-
-                var dt1 = DateTime.Now;
+               
                 BackgroundWorker wrkr = new BackgroundWorker();
                 wrkr.DoWork += (object sender, DoWorkEventArgs e) =>
                 {
-                    while (true)
-                    {
-                        if(StaticVal.binanceSocketClient.IncomingKbps == 0)
-                        {
-                            var dt2 = DateTime.Now;
-                            var div = dt2 - dt1;
-                            if(div.TotalSeconds > 60)
-                            {
-                                StaticVal.binanceSocketClient.UnsubscribeAllAsync();
-                                Thread.Sleep(2000);
-                                dt1 = DateTime.Now;
-                                var subscribe = StaticVal.binanceSocketClient.Spot.SubscribeToAllSymbolTickerUpdatesAsync(data => {
-                                    // Handle data when it is received
-                                    var lExists = binanceTick.Where(x => data.Data.Any(y => y.Symbol == x.Symbol));
-                                    if (lExists != null && lExists.Any())
-                                    {
-                                        binanceTick = binanceTick.Except(lExists).ToList();
-                                    }
-                                    binanceTick.AddRange(data.Data);
-                                    StaticVal.binanceTicks = binanceTick;
-                                });
-                                Console.WriteLine($"END: {dt2}");
-                            }
-                        }
-                        Thread.Sleep(1000);
-                    }
+                    var ticket = StaticVal.binanceClient.Spot.Market.GetTickersAsync().GetAwaiter().GetResult();
+                    StaticVal.binanceTicks = ticket.Data.ToList();
+                    wrkr.Dispose();
                 };
                 wrkr.RunWorkerAsync();
+                Thread.Sleep(2000);
+                new ScheduleMember(StaticVal.scheduleMng.GetScheduler(), JobBuilder.Create<SubcribeJob>(), "0/10 * * * * ?", nameof(SubcribeJob)).Start();
+
+                //var ticket = (await StaticVal.binanceClient.Spot.Market.GetTickersAsync()).Data.ToList();
+                //StaticVal.binanceTicks = ticket;
+                //var binanceTick = ticket;
+                //var subscribeResult = StaticVal.binanceSocketClient.Spot.SubscribeToAllSymbolTickerUpdatesAsync(data => {
+                //    // Handle data when it is received
+                //    var lExists = binanceTick.Where(x => data.Data.Any(y => y.Symbol == x.Symbol));
+                //    if (lExists != null && lExists.Any())
+                //    {
+                //        binanceTick = binanceTick.Except(lExists).ToList();
+                //    }
+                //    binanceTick.AddRange(data.Data);
+                //    StaticVal.binanceTicks = binanceTick;
+                //});
+
+                //var dt1 = DateTime.Now;
+                //BackgroundWorker wrkr = new BackgroundWorker();
+                //wrkr.DoWork += (object sender, DoWorkEventArgs e) =>
+                //{
+                //    while (true)
+                //    {
+                //        if(StaticVal.binanceSocketClient.IncomingKbps == 0)
+                //        {
+                //            var dt2 = DateTime.Now;
+                //            var div = dt2 - dt1;
+                //            if(div.TotalSeconds > 60)
+                //            {
+                //                StaticVal.binanceSocketClient.UnsubscribeAllAsync();
+                //                Thread.Sleep(2000);
+                //                dt1 = DateTime.Now;
+                //                var subscribe = StaticVal.binanceSocketClient.Spot.SubscribeToAllSymbolTickerUpdatesAsync(data => {
+                //                    // Handle data when it is received
+                //                    var lExists = binanceTick.Where(x => data.Data.Any(y => y.Symbol == x.Symbol));
+                //                    if (lExists != null && lExists.Any())
+                //                    {
+                //                        binanceTick = binanceTick.Except(lExists).ToList();
+                //                    }
+                //                    binanceTick.AddRange(data.Data);
+                //                    StaticVal.binanceTicks = binanceTick;
+                //                });
+                //                Console.WriteLine($"END: {dt2}");
+                //            }
+                //        }
+                //        Thread.Sleep(1000);
+                //    }
+                //};
+                //wrkr.RunWorkerAsync();
             }
             catch(Exception ex)
             {
